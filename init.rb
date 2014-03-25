a38bf7253ca11a38a7a473fa49dfe39134d7f3ae
Dario <Smogg@MacBook-Pro-di-Dario-Benati.local>
2014-03-25 14:21:55 +0100

    Rewriting Views Plugin, first commit.

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :rewriting_views do
+  name 'Rewriting Views plugin'
+  author 'Francesco Meli'
+  description 'This plugin rewrite specific Redmine views'
+  version '0.0.1'
+  url 'http://www.francescomeli.com/'
+  author_url 'http://www.francescomeli.com/'
+end
